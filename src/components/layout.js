1ff1d4cc3771c6f3ac6961391f1c619aabd71fce
michael dimmitt <michaelgdimmitt@gmail.com>
2019-03-21 17:56:43 -0400

    simplify formatting for readability

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,24 +16,12 @@ const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
+        site { siteMetadata { title } } }
     `}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div style={{ margin: `0 auto`, maxWidth: 960, padding: `0px 1.0875rem 1.45rem`, paddingTop: 0 }} >
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
--- src/components/seo.js
+++ src/components/seo.js
@@ -28,53 +28,22 @@ function SEO({ description, lang, meta, keywords, title }) {
   const metaDescription = description || site.siteMetadata.description
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
+    <Helmet htmlAttributes={{ lang, }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
+      meta={[ 
+        { name: `description`, content: metaDescription },
+        { property: `og:title`, content: title },
+        { property: `og:description`, content: metaDescription, },
+        { property: `og:type`, content: `website` },
+        { name: `twitter:card`, content: `summary` },
+        { name: `twitter:creator`, content: site.siteMetadata.author },
+        { name: `twitter:title`, content: title },
+        { name: `twitter:description`, content: metaDescription },
       ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
-              }
-            : []
+        .concat( keywords.length > 0 
+          ? { name: `keywords`, content: keywords.join(`, `) } 
+          : []
         )
         .concat(meta)}
     />
